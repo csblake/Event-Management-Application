c1e55d7d261287862e32f7e0278fb37918293bd3
Clayton Blake <clay.blake9@gmail.com>
2022-04-11 16:49:04 -0500

    Project Setup
    Made all the files and pathways that should be needed to complete the project.

---
+++ Event-Management-Application/src/main/java/dmacc/EventManagementApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementApplication.class, args);
+	}
+
+}
